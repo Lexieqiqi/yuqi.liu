cb56aaf44da221189ab452d148541166db272c1a
Lexieqiqi <lexieqiqi@gmail.com>
2022-06-26 14:56:22 +0800

    change base path

--- next.config.js
+++ next.config.js
@@ -4,5 +4,5 @@ module.exports = {
     loader: 'imgix', ///workaround to not optimize images for static exports
     path: '/yuqi.liu'//yuqi.liu'
   },
-  //basePath: '/yuqi.liu'
+  basePath: '/yuqi.liu'
 }
