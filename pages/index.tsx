1632c05f72d0befc09f23dc22d717a3ae43548f2
Lexieqiqi <lexieqiqi@gmail.com>
2022-06-26 12:17:54 +0800

    add main pic

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,23 +39,28 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <div className={styles.intro_pic_container}>
+            <div className={styles.intro_pic}>
+                <Image src="/intro_pic/bus_2.jpeg" 
+                        alt="my story"
+                        className={styles.ip}
+                        width={500}
+                        height={450} 
+                        layout="fixed"/>
+            </div>
+        </div>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">my wonderland!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+            <h2>Projects &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
+            <h2>Blog &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
@@ -63,7 +68,7 @@ export default function Home() {
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
+            <h2>Story &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
@@ -71,7 +76,7 @@ export default function Home() {
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Gallery &rarr;</h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
