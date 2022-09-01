0506d9ee73a33f52c2b232821756ad168f1bfe59
Lexieqiqi <lexieqiqi@gmail.com>
2022-09-01 16:59:27 +0800

    not again...

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   images: {
     loader: 'imgix', ///workaround to not optimize images for static exports
-    path: '/'//yuqi.liu'
+    path: '/yuqi.liu'//yuqi.liu'
   },
-  basePath: '' //yuqi.liu
+  basePath: '/yuqi.liu' //yuqi.liu
 }
